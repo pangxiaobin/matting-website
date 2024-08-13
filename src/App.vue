7fbf2bab865d724187d63cc28c749ea595540842
lanford1204 <panglaibin2013@163.com>
2024-08-13 17:18:55 +0800

    feat:add vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 import Home from '@/views/Home/index.vue'
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
 
 <template>
   <Home />
+  <SpeedInsights />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import i18n from './locales/index.js'; // 确保路径正确
+import { inject } from "@vercel/analytics"
 
+inject();
 const app = createApp(App);
 app.use(i18n).mount('#app');
