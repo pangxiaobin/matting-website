201f0c25a3b929de5af2fe05b4b9dbe76b36d2cf
lanford1204 <panglaibin2013@163.com>
2024-10-11 13:32:20 +0800

    add version api

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home/index.vue'
 import Version from '@/views/Home/components/Version.vue'
+import versionInfo from '@/utils/version.js';
 
 const routes = [
   { path: '/', component: Home },
   {
     path: '/version',
     component: Version,
+  },
+  {
+    path: '/api/version',
+    name: 'VersionAPI',
+    beforeEnter(to, from, next) {
+      // 使用原生 JavaScript 实现 JSON 响应
+      const jsonResponse = JSON.stringify(versionInfo);
+      const blob = new Blob([jsonResponse], { type: 'application/json' });
+      const url = URL.createObjectURL(blob);
+
+      // 将请求响应设置为 JSON
+      window.location.href = url;
+    }
   },
   {
     path: '/:pathMatch(.*)*', 
