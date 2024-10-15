8eb263fd8858df2383cbb6a5bffd65bcc391475b
lanford1204 <panglaibin2013@163.com>
2024-10-15 21:04:29 +0800

    chore:updare version info

--- src/views/Home/components/Version.vue
+++ src/views/Home/components/Version.vue
@@ -1,21 +1,23 @@
 <template>
-    <div class="version bg-gray-100 p-6 rounded-lg shadow-lg max-w-md mx-auto my-8">
-      <h2 class="text-2xl font-semibold text-gray-800 mb-4">Version</h2>
-      <p class="text-lg text-gray-600 font-medium mb-2">最新版本 : {{ versionInfo.version }}</p>
+  <div class="version bg-gray-100 p-6 rounded-lg shadow-lg max-w-md mx-auto my-8">
+    <h2 class="text-2xl font-semibold text-gray-800 mb-4">Version</h2>
+    <div v-for="(versionInfo, idx) in versionInfoList" :key="idx" class="mt-8">
+      <p class="text-lg text-gray-600 font-medium mb-2">版本 : {{ versionInfo.version }}</p>
       <p class="text-lg text-gray-600 font-medium mb-4"> 发布日期 : {{ versionInfo.date }}</p>
-      <p class="text-lg text-gray-600 font-medium mb-2">公众号 : 努力的PLb 获取最新版本</p>
       <p class="text-lg text-gray-600 font-medium mb-2">更新内容 : </p>
       <ul class="list-disc pl-6 space-y-2">
-        <li v-for="(line, index) in description" :key="index" class="text-gray-700">{{ line }}</li>
+        <li v-for="(line, index) in formatDescription(versionInfo.description)" :key="index" class="text-gray-700">{{ line }}</li>
       </ul>
     </div>
-  </template>
-  
-  <script setup>
-  import versionInfo from '@/utils/version.js';
-  const description = versionInfo.description.split(';').map(line => line.trim());
-  </script>
-  
-  <style scoped>
-  </style>
-  
+
+  </div>
+</template>
+
+<script setup>
+import versionInfoList from '@/utils/version.js';
+const formatDescription = (description) => {
+  return description.split(';').map(line => line.trim());
+}
+</script>
+
+<style scoped></style>
