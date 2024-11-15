3c94ea42009b1bc29eb5ff4ed76dc1af05fe608b
lanford1204 <panglaibin2013@163.com>
2024-11-15 09:16:12 +0800

    update

--- src/views/Home/components/Download.vue
+++ src/views/Home/components/Download.vue
@@ -33,10 +33,10 @@
                     class="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                     <img src="https://www.favicon.vip/get.php?url=https://drive.uc.cn/" class="w-4 h-4 mr-2" alt=""> {{ t('download.uc') }}
                 </a>
-                <!-- <a href=" https://pan.xunlei.com/s/VO9NJyJSv0AvWXG6PWgfYT8VA1?pwd=twy8" target="_blank"
+                <a href=" https://pan.xunlei.com/s/VO9NJyJSv0AvWXG6PWgfYT8VA1?pwd=twy8" target="_blank"
                     class="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                     <img src="https://www.xunlei.com/favicon-16x16.png" class="w-4 h-4 mr-2" alt=""> {{ t('download.xunlei') }}
-                </a>    -->
+                </a>   
             </div>
         </div>
 
